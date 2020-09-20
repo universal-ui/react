5a91929f5846c5a3d74aab71478dce9d56e3fa93
cloudle <lehaoson@gmail.com>
2020-09-20 16:35:45 +0700

    initial commit, use lerna to manage child dependencies

---
+++ example/index.js
@@ -0,0 +1,3 @@
+import { greeting, } from '@react-universal-ui/components';
+
+greeting();
---
+++ packages/components/index.js
@@ -0,0 +1,5 @@
+import { pi, } from '@react-universal-ui/core';
+
+export const greeting = () => {
+	console.log('Pi\'s value is: ', pi);
+};
